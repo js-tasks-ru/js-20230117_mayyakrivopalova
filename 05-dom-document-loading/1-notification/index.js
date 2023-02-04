c9150b877518ada2f0ede0ef8f890ba33d75f4ec
safym <osafym@gmail.com>
2023-02-04 19:40:26 +0300

    add 05-1 task solution

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,62 @@
 export default class NotificationMessage {
+  static lastElement = {};
 
+  constructor(contentText = '', {
+    duration = 0,
+    type = 'success'
+  } = {}) {
+    this.contentText = contentText;
+    this.duration = duration;
+    this.type = type;
+    this.element = {};
+  }
+  show() {
+    if (Object.keys(NotificationMessage.lastElement).length) {
+      NotificationMessage.lastElement.remove();
+    }
+
+    this.render()
+      
+    NotificationMessage.lastElement = this;
+  }
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+    document.body.append(this.element)
+    setTimeout(() => {
+      this.remove();
+    }, this.duration)
+  }
+
+  getTemplate() {
+    return `
+      <div class="notification success" style="--value:${this.getFormattedDuration()}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.contentText}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getFormattedDuration() {
+    const sec = this.duration / 1000;
+    return `${sec}s`
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = {};
+    NotificationMessage.lastElement = {};
+  }
 }
